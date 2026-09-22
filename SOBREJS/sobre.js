f30c95ec1d1ecd35962db9d8bab464e293a5df2a
cfaillafranco-dot <cfaillafranco@gmail.com>
2026-09-22 18:51:34 -0300

    aba sobre quase finalizada

--- SOBREJS/sobre.js
+++ SOBREJS/sobre.js
@@ -1,4 +1,71 @@
 function mostrarAposentadoria1() {
     
     let texto = document.getElementById("aposentadoria1");
+
+
+    if (texto.style.display == "none") {
+        texto.style.display = "block";
+    } else {
+        texto.style.display = "none";
+    }
+}
+
+function mostrarPensão() {
+
+    let texto2 = document.getElementById("pensao");
+
+
+    if (texto2.style.display == "none") {
+        texto2.style.display = "block";
+    } else {
+        texto2.style.display = "none";
+    }
+}
+
+function mostrarAuxílio() {
+
+    let texto3 = document.getElementById("auxilio");
+
+
+    if (texto3.style.display == "none") {
+        texto3.style.display = "block";
+    } else {
+        texto3.style.display = "none";
+    }
+}
+
+function mostrarRevisão() {
+
+    let texto4 = document.getElementById("revisao");
+
+
+    if (texto4.style.display == "none") {
+        texto4.style.display = "block";
+    } else {
+        texto4.style.display = "none";
+    }
+}
+
+function mostrarAposentadoria2() {
+
+    let texto5 = document.getElementById("aposentadoria2");
+
+
+    if (texto5.style.display == "none") {
+        texto5.style.display = "block";
+    } else {
+        texto5.style.display = "none";
+    }
+}
+
+function mostrarAposentadoria3() {
+
+    let texto6 = document.getElementById("aposentadoria3");
+
+
+    if (texto6.style.display == "none") {
+        texto6.style.display = "block";
+    } else {
+        texto6.style.display = "none";
+    }
 }
